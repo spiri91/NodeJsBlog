a048e2f5766229b257981e566f1aaed2c0ba6ffa
Ionut Spataru <spataru.ionut91@yahoo.com>
2019-01-04 21:23:00 +0200

    we are live

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ require('dotenv').config({ path: '.env' });
 const express = require('express');
 const bodyParser = require('body-parser');
 const webpush = require('web-push');
-const fs = require('fs')
-const https = require('https')
-const http = require('http')
 
 const checkError = require('./misc/checkErrorResponse');
 const articlesRouter = require('./api/articles/articles');
@@ -13,15 +10,6 @@ const subscriber = require('./api/subscriptions/subscribe');
 
 webpush.setVapidDetails(process.env.NOTIFICATIONSUBJECT,
   process.env.PUBLICVAPIDKEY, process.env.PRIVATEVAPIDKEY);
-
-let privateKey = fs.readFileSync('sslCert/server.key', 'utf8');
-let certificate = fs.readFileSync('sslCert/server.cert', 'utf8');
-let credentials = {
-  key: privateKey,
-  cert: certificate,
-  requestCert: false,
-  rejectUnauthorized: false
-};
 
 const app = express();
 
@@ -40,10 +28,6 @@ app.use((err, req, res, next) => {
   checkError.checkErrorResponse({ code: err.message }, res);
 });
 
-let httpServer = http.createServer(app);
-let httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(process.env.HTTP_PORT);
-httpsServer.listen(process.env.HTTPS_PORT);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
